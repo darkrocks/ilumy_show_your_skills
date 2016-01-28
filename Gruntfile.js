94db0c21977c834ea1a11dbd62e8a6e78def8262
kazinov <kazinov.dima@gmail.com>
2016-01-28 14:08:01 +0700

    frontend started

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function (grunt) {
   // load plugins
   grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // config
   grunt.initConfig({
@@ -14,8 +16,26 @@ module.exports = function (grunt) {
           reporter: 'spec'
         }
       }
+    },
+    browserify: {
+      dist: {
+        files: {
+          'build/js/bundle.js': ['lib/public/js/app.js']
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {src: ['lib/public/index.html'], dest: 'build/index.html'},
+          {src: ['node_modules/bootstrap/dist/css/bootstrap.min.css'], dest: 'build/css/bootstrap.min.css'},
+          {expand: true, flatten: true, src: ['lib/public/views/**'], dest: 'build/views/', filter: 'isFile'},
+          {expand: true, flatten: true, src: ['lib/public/css/**'], dest: 'build/css/', filter: 'isFile'},
+        ]
+      }
     }
   });
 
   grunt.registerTask('test', 'mochacli:all')
+  grunt.registerTask('build', ['browserify', 'copy:main'])
 };
